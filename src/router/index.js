167d943d4814e3abce138e5e5e4bcecaa585a6fb
np943 <dlgustjr1024@naver.com>
2022-12-01 10:17:52 +0900

    수정?

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { 
-    createRouter, 
-    createWebHistory 
-  } from 'vue-router'
+  createRouter, 
+  createWebHistory 
+} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DataBinding from '../views/DataBinding.vue'
 //import AboutView from '../views/AboutView.vue'
@@ -16,111 +16,119 @@ import DatabindingRadio from '../views/DatabindingRadio.vue'
 import DataBindingAttribute from '../views/DataBindingAttribute.vue'
 import DataBindingButton from '../views/DataBindingButton.vue'
 import DataBindingClass from '../views/DataBindingClass.vue'
-import DataBindingClass2 from '../views/DataBindingClass2.vue'
-import DataBindingStyle from '../views/DataBindingStyle.vue'
-import DataBindingStyle2 from '../views/DataBindingStyle2.vue'
 import DataBindingStyle from '../views/DataBindingStyle.vue'
 import DataBindingStyle2 from '../views/DataBindingStyle2.vue'
 import DataBindingList from '../views/DataBindingList.vue'
 import RenderingVif from '../views/RenderingVIf.vue'
 import RenderingVShow from '../views/RenderingVShow.vue'
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackPrefetch : true, webpackChunkName: "about" */ '../views/AboutView.vue')
-                    
-    //component : AboutView
-  },
-  {
-    path: '/databinding',
-    name: 'DataBinding',
-    //component: () => import('../views/DataBinding.vue')
-    component: DataBinding
-  },
-  {
-    path: '/databindinghtml',
-    name: 'DataBindingHtml',
-    component: DataBindingHtml
-  },
-  {
-    path: '/databindinginputtext',
-    name: DataBindingInputText,
-    component: DataBindingInputText
-  },
-  {
-    path: '/databindinginputnumber',
-    name: DataBindingInputNumber,
-    component: DataBindingInputNumber
-  },
-  {
-    path: '/databindingtextarea',
-    name: DataBindingTextarea,
-    component: DataBindingTextarea
-  },
-  {
-    path: '/databindingselect',
-    name: DataBindingSelect,
-    component: DataBindingSelect
-  },
-  {
-    path: '/databindingscheckbox',
-    name: DataBindingCheckBox,
-    component: DataBindingCheckBox
-  },
-  {
-    path: '/databindingscheckbox2',
-    name: DataBindingCheckBox2,
-    component: DataBindingCheckBox2
-  },
-  {
-    path: '/databindingradio',
-    name: DatabindingRadio,
-    component: DatabindingRadio
-  },
-  {
-    path: '/databindinattribute',
-    name: DataBindingAttribute,
-    component: DataBindingAttribute
-  },
-  {
-    path: '/databindingbutton',
-    name: DataBindingButton,
-    component: DataBindingButton
-  },
-  {
-    path: '/databindingclass',
-    name: DataBindingClass,
-    component: DataBindingClass
-  },
-  {
-    path: '/databindingclass2',
-    name: DataBindingClass2,
-    component: DataBindingClass2
-  }  ,
-  {
-    path: '/databindingstyleobject',
-    name: DataBindingStyle,
-    component: DataBindingStyle
-  }  ,
-  {
-    path: '/databindingstyleobject2',
-    name: DataBindingStyle2,
-    component: DataBindingStyle2
-  }
+{
+  path: '/',
+  name: 'Home',
+  component: HomeView
+},
+{
+  path: '/about',
+  name: 'About',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackPrefetch : true, webpackChunkName: "about" */ '../views/AboutView.vue')
+                  
+  //component : AboutView
+},
+{
+  path: '/databinding',
+  name: 'DataBinding',
+  //component: () => import('../views/DataBinding.vue')
+  component: DataBinding
+},
+{
+  path: '/databindinghtml',
+  name: 'DataBindingHtml',
+  component: DataBindingHtml
+},
+{
+  path: '/databindinginputtext',
+  name: DataBindingInputText,
+  component: DataBindingInputText
+},
+{
+  path: '/databindinginputnumber',
+  name: DataBindingInputNumber,
+  component: DataBindingInputNumber
+},
+{
+  path: '/databindingtextarea',
+  name: DataBindingTextarea,
+  component: DataBindingTextarea
+},
+{
+  path: '/databindingselect',
+  name: DataBindingSelect,
+  component: DataBindingSelect
+},
+{
+  path: '/databindingscheckbox',
+  name: DataBindingCheckBox,
+  component: DataBindingCheckBox
+},
+{
+  path: '/databindingscheckbox2',
+  name: DataBindingCheckBox2,
+  component: DataBindingCheckBox2
+},
+{
+  path: '/databindingradio',
+  name: DatabindingRadio,
+  component: DatabindingRadio
+},
+{
+  path: '/databindinattribute',
+  name: DataBindingAttribute,
+  component: DataBindingAttribute
+},
+{
+  path: '/databindingbutton',
+  name: DataBindingButton,
+  component: DataBindingButton
+},
+{
+  path: '/databindingclass',
+  name: DataBindingClass,
+  component: DataBindingClass
+},
+{
+  path: '/databindingstyle',
+  name: DataBindingStyle,
+  component: DataBindingStyle
+},
+{
+  path: '/databindingstyle2',
+  name: DataBindingStyle2,
+  component: DataBindingStyle2
+},
+{
+  path: '/databindinglist',
+  name: DataBindingList,
+  component: DataBindingList
+},
+{
+  path: '/renderingvif',
+  name: RenderingVif,
+  component: RenderingVif
+},
+{
+  path: '/renderingvshow',
+  name: RenderingVShow,
+  component: RenderingVShow
+}
+
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+history: createWebHistory(process.env.BASE_URL),
+routes
 })
 
 export default router
